05c878b76e01b5134963d6b0b56e7b6be0624fd7
Matías Nicolás Zárate Araya <matiaszaratearaya@gmail.com>
2026-04-21 10:07:57 -0400

    proyecto link-saver

--- src/app/app.ts
+++ src/app/app.ts
@@ -15,5 +15,6 @@ export class App {
   protected readonly projects = [
     { title: 'Home Page', link: 'https://github.com/mnza-na/home-page', short_description: 'Carta de presentacion a mis proyectos', icon: 'logo-1.svg'},
     { title: 'Glue Job Examples', link: 'https://github.com/mnza-na/aws-glue-examples', short_description: 'Ejemplos para reutilizacion de jobs de glue', icon: 'glue.webp'},
+    { title: 'Link Saver', link: 'https://github.com/mnza-na/link-saver', short_description: 'Un pequeño back + front para almacenar links que utilizo recurrentemente (en curso)', icon: 'public/image-not-found.png'},
   ]
 }
